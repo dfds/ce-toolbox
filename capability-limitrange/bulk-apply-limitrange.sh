8b8f69d49ad3ee45c179e0fc452691a1c88be4b8
Richard Fisher <rifis@dfds.com>
2021-09-01 14:33:14 +0100

    script modification and first exception - modified script to allow making exceptions/overrides - increase cpu limit on terminal-cargoallocati-zamrl - added simple README

--- capability-limitrange/bulk-apply-limitrange.sh
+++ capability-limitrange/bulk-apply-limitrange.sh
@@ -2,11 +2,19 @@
 
 set -euo pipefail
 
-# Get all namespaces, but exclude kube-system and monitoring
-NAMESPACES=$(kubectl get ns --no-headers=true | awk '!/kube-system|monitoring/{print $1}')
+# Get all namespaces, but exclude kube-system and monitoring plus namespaces that have exceptions
+NAMESPACES=$(kubectl get ns --no-headers=true | awk '!/kube-system|monitoring|terminal-cargoallocati-zamrl/{print $1}')
 
+# Apply to all namespaces
 for NS in $NAMESPACES
 do
 	echo "Applying limitrange in namespace: $NS"
 	kubectl -n $NS apply -f limitrange.yaml
+done
+
+# Apply exceptions
+for file in `ls exceptions | grep .yaml`
+do
+	echo "Applying: $file"
+	kubectl apply -f exceptions/$file
 done
