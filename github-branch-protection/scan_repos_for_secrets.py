eaa72a9cc1f23d620a79bd02f04d86f81b454975
Audun Nes <aunes@dfds.com>
2022-01-12 11:05:03 +0100

    Refactor and use logging
    Signed-off-by: Audun Nes <aunes@dfds.com>

--- github-branch-protection/scan_repos_for_secrets.py
+++ github-branch-protection/scan_repos_for_secrets.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import logging
 import os
 import subprocess
 
@@ -12,28 +13,29 @@ def main():
     Requirements that needs to be pre-installed:
     - git
     - https://github.com/zricethezav/gitleaks
-    - https://github.com/kootenpv/gittyleaks
     """
 
     token: str = os.environ.get('GITHUB_OAUTH2_TOKEN')
     repo: Repo = Repo(token=token, owner=GITHUB_ORGANIZATION)
-    repo_list: list = repo.get_private_repos()
+    repo_list: list = repo.get_all_repos()
 
-    tmp_clone_dir: str = "/tmp/gitleaks/code"
-    tmp_report_dir: str = "/tmp/gitleaks/logs"
-    os.makedirs(tmp_clone_dir, exist_ok=True)
-    os.makedirs(tmp_report_dir, exist_ok=True)
+    _clone_dir: str = "/tmp/code"
+    _gitleaks_report_dir: str = "/tmp/logs/gitleaks"
+    os.makedirs(_clone_dir, exist_ok=True)
+    os.makedirs(_gitleaks_report_dir, exist_ok=True)
 
     for repos in repo_list:
         for r in repos:
             name: str = r.get('name')
             ssh_url: str = r.get('ssh_url')
-            git_clone: str = f'git clone {ssh_url}'
-            subprocess.run(git_clone.split(' '), cwd=tmp_clone_dir)
-            git_leaks: str = f'gitleaks detect -r {tmp_report_dir}/{name}.json'
-            output = subprocess.run(git_leaks.split(' '), cwd=f'{tmp_clone_dir}/{name}', capture_output=True)
-            if output.returncode == 0:  # We only want to store a report when issues are found
-                subprocess.run(["rm", "-f", f'{tmp_report_dir}/{name}.json'])
+            logging.info(f'Cloning repository {name}')
+            _cmd_git_clone: str = f'git clone {ssh_url}'
+            subprocess.run(_cmd_git_clone.split(' '), cwd=_clone_dir)
+            logging.info(f'Scanning {name} with gitleaks')
+            _cmd_gitleaks: str = f'gitleaks detect -r {_gitleaks_report_dir}/{name}.json'
+            _gitleaks_output = subprocess.run(_cmd_gitleaks.split(' '), cwd=f'{_clone_dir}/{name}', capture_output=True)
+            if _gitleaks_output.returncode == 0:  # Only store a report when issues are found
+                subprocess.run(["rm", "-f", f'{_gitleaks_report_dir}/{name}.json'])
 
 
 if __name__ == "__main__":
