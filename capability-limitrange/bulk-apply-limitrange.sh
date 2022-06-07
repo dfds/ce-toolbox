048caae2dcf89fb66ae625229d0f9a0f7942acd4
Audun V. Nes <aunes@dfds.com>
2022-06-07 10:30:50 +0200

    Excluse Treafik namespace from limitranges

--- capability-limitrange/bulk-apply-limitrange.sh
+++ capability-limitrange/bulk-apply-limitrange.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Get all namespaces, but exclude kube-system and monitoring plus namespaces that have exceptions
-NAMESPACES=$(kubectl get ns --no-headers=true | awk '!/kube-system|monitoring|terminal-cargoallocati-zamrl/{print $1}')
+NAMESPACES=$(kubectl get ns --no-headers=true | awk '!/kube-system|monitoring|traefik|terminal-cargoallocati-zamrl/{print $1}')
 
 # Apply to all namespaces
 for NS in $NAMESPACES
