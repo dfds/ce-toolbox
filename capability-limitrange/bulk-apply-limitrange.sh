0647f605be2e2655dbad7797c326bd9d1feb8509
Willi Carlsen <carlsenwilli@gmail.com>
2022-06-14 10:36:01 +0200

    New limit ranges applied

--- capability-limitrange/bulk-apply-limitrange.sh
+++ capability-limitrange/bulk-apply-limitrange.sh
@@ -3,18 +3,11 @@
 set -euo pipefail
 
 # Get all namespaces, but exclude kube-system and monitoring plus namespaces that have exceptions
-NAMESPACES=$(kubectl get ns --no-headers=true | awk '!/kube-system|monitoring|traefik|terminal-cargoallocati-zamrl/{print $1}')
+NAMESPACES=$(kubectl get ns --no-headers=true | awk '!/kube-system|monitoring|traefik|kyverno|fluentd|flux-system|velero|atlantis|upbound-system|terminal-cargoallocati-zamrl/{print $1}')
 
 # Apply to all namespaces
 for NS in $NAMESPACES
 do
 	echo "Applying limitrange in namespace: $NS"
 	kubectl -n $NS apply -f limitrange.yaml
-done
-
-# Apply exceptions
-for file in `ls exceptions | grep .yaml`
-do
-	echo "Applying: $file"
-	kubectl apply -f exceptions/$file
 done
