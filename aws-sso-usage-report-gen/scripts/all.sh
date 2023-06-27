afa3d0ff9ef7cf1b8fe245d697f9e165a08cccb4
Audun V. Nes <aunes@dfds.com>
2023-06-27 09:20:59 +0200

    Revert "Added AWS SSO usage report scripts (#63)" (#81)
    This reverts commit fac9bded266e5f3597360d7854b90e8f232c3ed5.

--- aws-sso-usage-report-gen/scripts/all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-go run ../main.go general -i $1 | qsv table
-go run ../main.go user-country -i $1 | qsv table
-go run ../main.go user-activity -i $1 | qsv sort -s totalSignIns -R -N | qsv table
