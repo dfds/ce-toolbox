d1108f346bcdc4a87eece78468ed95f3003cd573
Audun Nes <aunes@dfds.com>
2024-01-04 11:10:46 +0100

    Exclue namespace codeanalysis-yjlbr from limitranges

--- capability-limitrange/bulk-apply-limitrange.sh
+++ capability-limitrange/bulk-apply-limitrange.sh
@@ -3,11 +3,10 @@
 set -euo pipefail
 
 # Get all namespaces, but exclude kube-system and monitoring plus namespaces that have exceptions
-NAMESPACES=$(kubectl get ns --no-headers=true | awk '!/kube-system|monitoring|traefik|kyverno|fluentd|flux-system|velero|atlantis|upbound-system|terminal-cargoallocati-zamrl/{print $1}')
+NAMESPACES=$(kubectl get ns --no-headers=true | awk '!/kube-system|monitoring|traefik|kyverno|fluentd|flux-system|velero|atlantis|upbound-system|terminal-cargoallocati-zamrl|codeanalysis-yjlbr/{print $1}')
 
 # Apply to all namespaces
-for NS in $NAMESPACES
-do
+for NS in $NAMESPACES; do
 	echo "Applying limitrange in namespace: $NS"
 	kubectl -n $NS apply -f limitrange.yaml
 done
