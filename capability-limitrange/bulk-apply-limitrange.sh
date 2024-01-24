d6eb4a6a61cb1500826ea88168f1a164a80f3ab4
Aleksandra Fromm <61739075+aleksandra-fromm@users.noreply.github.com>
2024-01-24 10:25:47 +0100

    Exclude namespaces from limitranges (#93)

--- capability-limitrange/bulk-apply-limitrange.sh
+++ capability-limitrange/bulk-apply-limitrange.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Get all namespaces, but exclude kube-system and monitoring plus namespaces that have exceptions
-NAMESPACES=$(kubectl get ns --no-headers=true | awk '!/kube-system|monitoring|traefik|kyverno|fluentd|flux-system|velero|atlantis|upbound-system|terminal-cargoallocati-zamrl|codeanalysis-yjlbr/{print $1}')
+NAMESPACES=$(kubectl get ns --no-headers=true | awk '!/kube-system|monitoring|traefik|kyverno|fluentd|flux-system|velero|atlantis|upbound-system|terminal-cargoallocati-zamrl|codeanalysis-yjlbr|onboardcustomers-npxkm|logistics-quotes-eqxam|logistics-routes-bapbj|logistics-quote-stagin-zlpxz/{print $1}')
 
 # Apply to all namespaces
 for NS in $NAMESPACES; do
